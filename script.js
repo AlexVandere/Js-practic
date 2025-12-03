868dec0dd7156e34a9066bcc934d400abadc8f8e
Alexander <koxnoo@gmail.com>
2025-12-03 14:25:16 +0200

    home work

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-let userPrompt = +prompt('ведіть число від 1 до 4 ')
-let result 
-switch (userPrompt) {
-    case 1:
-        result = 'зима'
-        break;
-    case 2:
-        result = 'весна'
-        break;
-    case 3:
-        result = 'літо'
-        break;
-    case 4:
-        result = 'осінь'
-        break
-    default:
-        result ='ведіть будь ласка друге значення'
+function geNumber(min, max) {
+    let sum = 0
+    for (let i = min; i <= max; i++){
+        if (i % 2 === 0) { 
+           sum += i
+        }
+    }
+    return sum
 }
-alert(result)
+ 
+console.log(geNumber(0,34))
